0f4f8ac90d99c64fdb44a0f29ac1e58dc14e4c4c
Jason Chuang <chuangtcee@gmail.com>
2024-03-19 10:21:01 +0800

    bump to v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='click_app',
-    version='0.1.0',
+    version='0.1.1',
     author='Jason Chuang',
     author_email='chuangtcee@gmail.com',
     description='A template of Click application CLI',
